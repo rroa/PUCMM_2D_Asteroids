dc132f227bf372cdbdf761296ae1889b1fa102a6
Raúl Roa <raul.roa@gmail.com>
2016-05-29 22:00:50 -0500

    Refactor/game structure (#4)
    * Re-factored code to now reflect a game like structure.
    
    * Re-factored code to now reflect a game like structure.
    
    * Rebasing master and using the same type on width and height variables and parameters
    
    * Minor changes
    
    * Removed debug directories. Refactored client code to check for init before executing the game, also changed declaration from stack to heap. Modified OnExecute and removed OnInit so it has to be called before
    
    * Renamed enum entries. Added game state validation before executing.

---
+++ Asteroids/Game.cpp
@@ -0,0 +1,166 @@
+#include "Game.hpp"
+#include <iostream>
+
+// OpenGL includes
+#include <GL\glew.h>
+#include <SDL2\SDL_opengl.h>
+
+namespace Engine
+{
+	Game::Game(const std::string& title, const int width, const int height)
+		:	m_title ( title  ),
+			m_width ( width  ),
+			m_height( height )
+	{
+		m_mainWindow = nullptr;
+		m_state = GameState::UNINITIALIZED;
+	}
+
+	void Game::OnExecute()
+	{
+		if(m_state != GameState::INIT_SUCCESSFUL)
+		{ 
+			std::cerr << "Game INIT was not successful." << std::endl;
+			return;
+		}
+
+		m_state = GameState::RUNNING;
+
+		SDL_Event event;
+		while (m_state == GameState::RUNNING)
+		{
+			// Input polling
+			//
+			while (SDL_PollEvent(&event))
+			{}
+
+			//
+			OnUpdate();
+			OnRender();
+		}
+	}
+
+	bool Game::OnInit()
+	{
+		// Init the external dependencies
+		//
+		bool success = SDLInit() && GlewInit();
+		if (!success)
+		{
+			m_state = GameState::INIT_FAILED;
+			return false;
+		}
+
+		// Setup the viewport
+		//
+		SetupViewport();
+
+		// Change game state
+		//
+		m_state = GameState::INIT_SUCCESSFUL;
+
+		return true;
+	}
+
+	void Game::OnKeyDown(SDL_KeyboardEvent keyBoardEvent)
+	{}
+
+	void Game::OnKeyUp(SDL_KeyboardEvent keyBoardEvent)
+	{}
+
+	void Game::OnUpdate()
+	{}
+
+	void Game::OnRender()
+	{
+		glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(m_mainWindow);
+	}
+
+	bool Game::SDLInit()
+	{
+		// Initialize SDL's Video subsystem
+		//
+		if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+		{
+			std::cerr << "Failed to init SDL" << std::endl;
+			return false;
+		}
+
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+		m_mainWindow = SDL_CreateWindow(
+			m_title.c_str(),
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			m_width,
+			m_height,
+			SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+		);
+
+		if (!m_mainWindow)
+		{
+			std::cerr << "Failed to create window!" << std::endl;
+			SDL_Quit();
+			return false;
+		}
+
+		m_context = SDL_GL_CreateContext(m_mainWindow);
+		SDL_GL_MakeCurrent(m_mainWindow, m_context);
+
+		// Make double buffer interval synced with vertical scanline refresh
+		SDL_GL_SetSwapInterval(1);
+
+		return true;
+	}
+
+	void Game::SetupViewport()
+	{
+		// Defining ortho values
+		//
+		float halfWidth = m_width * 0.5f;
+		float halfHeight = m_height * 0.5f;
+
+		// Set viewport to match window
+		//
+		glViewport(0, 0, m_width, m_height);
+
+		// Set Mode to GL_PROJECTION
+		//
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+
+		// Set projection MATRIX to ORTHO
+		//
+		glOrtho(-halfWidth, halfWidth, -halfHeight, halfHeight, -1, 1);
+
+		// Setting Mode to GL_MODELVIEW
+		//
+		glMatrixMode(GL_MODELVIEW);
+	}
+
+	bool Game::GlewInit()
+	{
+		GLenum err = glewInit();
+		if (GLEW_OK != err)
+		{
+			std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+			return false;
+		}
+
+		return true;
+	}
+
+	void Game::CleanupSDL()
+	{
+		// Cleanup
+		//
+		SDL_GL_DeleteContext(m_context);
+		SDL_DestroyWindow(m_mainWindow);
+
+		SDL_Quit();
+	}
+}
--- Asteroids/Game.hpp
+++ Asteroids/Game.hpp
@@ -1,6 +1,59 @@
 #pragma once
+#ifndef GAME_HPP
+#define GAME_HPP
 
-class Game
+#include <string>
+#include <SDL2\SDL.h>
+
+namespace Engine
 {
+	class Game
+	{
+	public:
+		/* =============================================================
+		* ENUMERATORS
+		* ============================================================= */
+		struct GameState
+		{
+			enum State
+			{
+				UNINITIALIZED = 0,
+				INIT_FAILED = 1,
+				INIT_SUCCESSFUL = 2,
+				RUNNING = 4,
+				PAUSED = 8,
+				QUIT = 16
+			};
+		};
 
-};
+		/* =============================================================
+		 * PUBLIC FUNCTIONS
+		 * ============================================================= */
+		Game				( const std::string& title, const int width, const int height );
+		void OnExecute		( );
+		bool OnInit			( );
+		void OnUpdate		( );
+		void OnRender		( );
+		void OnKeyDown	    ( SDL_KeyboardEvent keyBoardEvent );
+		void OnKeyUp		( SDL_KeyboardEvent keyBoardEvent );
+	private:
+		/* =============================================================
+		 * PRIVATE FUNCTIONS
+		 * ============================================================= */
+		bool SDLInit		( );
+		bool GlewInit		( );
+		void SetupViewport	( );
+		void CleanupSDL		( );
+
+		/* =============================================================
+		 * MEMBERS
+		 * ============================================================= */
+		std::string			m_title;
+		int					m_width;
+		int					m_height;
+		SDL_Window*			m_mainWindow;
+		SDL_GLContext		m_context;
+		GameState::State	m_state;
+	};
+}
+#endif /* GAME_HPP */
